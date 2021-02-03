5ce2459d02ccbf639e0e841f46fe9fab2fe72486
72nd <msg@frg72.com>
2021-02-03 20:12:25 +0100

    setup.py initialized

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="ipybudget",
+    version="0.1.0",
+    author="72nd",
+    author_email="msg@frg72.com",
+    packages=setuptools.find_packages(),
+)
