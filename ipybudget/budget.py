4534356461a9608c718db2d974a8cc036ed339c3
72nd <msg@frg72.com>
2021-02-03 20:27:15 +0100

    budget module added

---
+++ ipybudget/budget.py
@@ -0,0 +1,13 @@
+"""
+The Budget module contains all budget class related stuff.
+"""
+
+
+class Budget:
+    """
+    The Budget class is the main class for every ipybudget based budget. It
+    contains the different expense and income groups and provides export
+    functionality for the whole budget. For displaying the each entry it's
+    advised to use the display functionality of the Groups and using Jupyter's
+    markdown blocks to express the structure of the budget.
+    """
